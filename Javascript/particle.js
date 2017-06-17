d51ac6467e1d91ae4132abd0f4c01480c50df6aa
Cyril Alvarez <cyrilalavrezadriaansen@gmail.com>
2017-06-17 21:21:41 +0200

    chaange particles

--- Javascript/particle.js
+++ Javascript/particle.js
@@ -8,17 +8,19 @@ function canvas() {
   var Particle, particleCount, particles, sketch, z, context;
 
   sketch = Sketch.create({
-    container: document.getElementById('home')
+    container: document.getElementById('canvas')
   });
 
 
   particles = [];
 
-  particleCount = 750;
+  particleCount = 300;
   sketch.strokeStyle = 'hsla(' + random(0, 120) + ', 100%, 47%, 1)';
+  sketch.fillStyle = 'hsla(' + random(0, 120)  + ', 100%, 47%, 1)';
   
   var time = setInterval(function () {
     sketch.strokeStyle = 'hsla(' + random(0, 120) + ', 100%, 47%, 1)';
+    sketch.fillStyle = 'hsla(' + random(0, 120)  + ', 100%, 47%, 1)';
   }, 2000);
 
   sketch.globalCompositeOperation = 'lighter';
@@ -28,26 +30,38 @@ function canvas() {
     this.y = random(sketch.height, sketch.height * 2);
     this.vx = 0;
     this.vy = -random(1, 10) / 5;
-    this.radius = this.baseRadius = 1;
+    this.radius = this.baseRadius = 5;
+    this.maxRadius = 30;
+    this.threshold = 150;
+    return this.hue = random(180, 240);
   };
 
   Particle.prototype = {
     update: function () {
       var dist, distx, disty, radius;
-     
       dist = sqrt(distx * distx + disty * disty);
-  
+      if (dist < this.threshold) {
+        radius = this.baseRadius + ((this.threshold - dist) / this.threshold) * this.maxRadius;
+        this.radius = radius > this.maxRadius ? this.maxRadius : radius;
+      } else {
+        this.radius = this.baseRadius;
+      }
       this.vx += (random(100) - 50) / 1000;
       this.vy -= random(1, 20) / 10000;
       this.x += this.vx;
       this.y += this.vy;
-      
+      if (this.x < -this.maxRadius || this.x > sketch.width + this.maxRadius || this.y < -this.maxRadius) {
+        this.x = random(sketch.width);
+        this.y = random(sketch.height + this.maxRadius, sketch.height * 2);
+        this.vx = 0;
+        return this.vy = -random(1, 10) / 5;
+      }
     },
     render: function () {
       sketch.beginPath();
       sketch.arc(this.x, this.y, this.radius, 0, TWO_PI);
       sketch.closePath();
-      sketch.fillStyle = 'hsla(' + this.hue + ', 60%, 40%, 0.5)';
+      sketch.fillStyle = 'hsla(' + random(0, 120)  + ', 100%, 47%, 1)';
       sketch.fill();
       return sketch.stroke();
     }
